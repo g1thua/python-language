0e745f08770f2e47190b1fba71a1d9cf3ee2b72d
g1thua <gilbertmwangi254@gmail.com>
2026-03-05 19:26:49 +0300

    first commit

---
+++ day_1/datatypes.py
@@ -0,0 +1,9 @@
+print ('hello', 'world')
+
+name = 'gilbert'
+age = 19
+print("my name is", name , "and I'm",age,"years old" )
+
+variable1= type(name)
+variable2 = type(age)
+print(variable1, variable2)
